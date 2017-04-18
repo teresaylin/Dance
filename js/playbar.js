3863b291ff4b63ba88faca7e25e76870d0226c0c
Alex Chen <username@mit.edu>
2017-04-17 23:58:10 -0400

    finished first version of playbar

--- js/playbar.js
+++ js/playbar.js
@@ -9,3 +9,31 @@ function play_playbar() {
         document.getElementById('play').className = "play";
     }
 }
+
+function time() {
+    var percent = 700 * (document.getElementById('player').currentTime / document.getElementById('player').duration);
+    document.getElementById('playmarker').style.marginLeft = percent + "px";
+}
+
+function addBubble(currentTime, formationNumber){
+    var leftMargin = 700 * currentTime / document.getElementById('player').duration+ "px";
+    $("#bubblebar").append("<div id='bubble' formnumber='" +formationNumber+"' time='"+currentTime+"' style='margin-left:" +leftMargin +"' onclick = 'onBubbleClick(this)'></div>");
+}
+
+function onBubbleClick(bubble){
+    var formationID = bubble.getAttribute("formnumber");
+    var time = bubble.getAttribute("time");
+
+    console.log(time);
+    console.log(document.getElementById('player').currentTime);
+    document.getElementById('player').currentTime = time;
+    //TODO add how to change slides when a bubble is clicked (I only handled the playbar)
+}
+
+
+
+function onBubbleClick2(e){
+    console.log(e);
+    console.log($(this)[0])
+}
+
